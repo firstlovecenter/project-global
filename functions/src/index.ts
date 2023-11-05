aba5ae3af1d2f4f18603181cb90772bebda24d1c
John-Dag Addy <jaedagy@gmail.com>
2023-11-05 00:47:08 +0000

    fix: update firebase settings

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,22 +7,15 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import { onRequest } from 'firebase-functions/v2/https'
-import * as logger from 'firebase-functions/logger'
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
-
 admin.initializeApp()
 
-export const pushToArray = functions.firestore
+export const updateRoleChurchesFromLeadsCampuses = functions.firestore
   .document('members/{memberId}/leadsCampuses/{campusId}')
   .onWrite(async (change, context) => {
     const { memberId, campusId } = context.params
