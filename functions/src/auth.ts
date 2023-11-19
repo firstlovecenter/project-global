3116a1ddb0f4da4891822e91f6e71ad0891774aa
John-Dag Addy <jaedagy@gmail.com>
2023-11-19 00:44:32 +0000

    fix: remove unused import and variable declaration

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -1,7 +1,4 @@
 import * as functions from 'firebase-functions'
-import * as admin from 'firebase-admin'
-
-const db = admin.firestore()
 
 export const createUserRecord = functions
   .region('europe-west1')
