358aa4418a17cca0d202bafebee5028387e469ca
John-Dag Addy <jaedagy@gmail.com>
2023-11-20 22:44:41 +0000

    fix: commented out createUserRecord function in auth.ts and index.ts

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -1,8 +1,8 @@
-import * as functions from 'firebase-functions'
+// import * as functions from 'firebase-functions'
 
-export const createUserRecord = functions
-  .region('europe-west1')
-  .auth.user()
-  .beforeSignIn((user) => {
-    console.log('🚀 ~ file: auth.ts:10 ~ user:', user)
-  })
+// export const createUserRecord = functions
+//   .region('europe-west1')
+//   .auth.user()
+//   .beforeSignIn((user) => {
+//     console.log('🚀 ~ file: auth.ts:10 ~ user:', user)
+//   })
