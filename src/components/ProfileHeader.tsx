85f74655fcde6c0ee4da430c9ea957810c92b29a
ohenekwabena <adjeiasarejesse@gmail.com>
2024-07-03 12:30:50 +0000

    feat(ui): set up themes and fonts for frontend

--- src/components/ProfileHeader.tsx
+++ src/components/ProfileHeader.tsx
@@ -7,27 +7,26 @@ function ProfileHeader({
   email,
 }: Readonly<{ name: string; email: string }>) {
   return (
-    <Box p={'1rem 0'} borderTop={'1px solid #96A7AF'}>
-      <Flex
-        gap={3}
-        bg={'#262A32'}
-        p={'0.65rem 0.5rem '}
-        alignItems={'center'}
-        borderRadius={'10px'}
-      >
-        <Box borderRadius={'5000px'} p={0} overflow={'hidden'} width={'40px'}>
-          <Img src="https://via.placeholder.com/150" />
-        </Box>
-        <Box>
-          <Text fontSize={'14px'} colorScheme="brandGray">
-            {name}
-          </Text>
-          <Text fontSize={'12px'} color={'#7B8488'}>
-            {email}
-          </Text>
-        </Box>
-      </Flex>
-    </Box>
+    <Flex
+      gap={3}
+      bg={'#262A32'}
+      p={'0.65rem 0.5rem '}
+      alignItems={'center'}
+      borderRadius={'10px'}
+      width={'100%'}
+    >
+      <Box borderRadius={'5000px'} p={0} overflow={'hidden'} width={'40px'}>
+        <Img src="https://via.placeholder.com/150" />
+      </Box>
+      <Box>
+        <Text fontSize={'14px'} colorScheme="brandGray">
+          {name}
+        </Text>
+        <Text fontSize={'12px'} color={'#7B8488'}>
+          {email}
+        </Text>
+      </Box>
+    </Flex>
   )
 }
 
