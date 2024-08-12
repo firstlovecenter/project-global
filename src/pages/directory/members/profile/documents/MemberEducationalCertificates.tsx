80bd16ada1f2a34cc8dfd23c3e3766b96e3fb294
ohenekwabena <adjeiasarejesse@gmail.com>
2024-08-12 20:43:17 +0000

    feat(ui): implement member document upload

--- src/pages/directory/members/profile/documents/MemberEducationalCertificates.tsx
+++ src/pages/directory/members/profile/documents/MemberEducationalCertificates.tsx
@@ -1,9 +1,20 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
+import { RootState } from 'redux-config/store'
+import DocumentUpload from './components/DocumentUpload'
+import ProfileAvatar from './components/ProfileAvatar'
+import { useSelector } from 'react-redux'
 
 const MemberEducationalCertificates = () => {
+  const member = useSelector((state: RootState) => state.member.data)
   return (
     <Container>
-      <Heading textAlign={'center'}>Educational Certificates</Heading>
+      <Heading textAlign={'center'} mb={10}>
+        Educational Certificates
+      </Heading>
+      <ProfileAvatar member={member} />
+      <Box mt={10}>
+        <DocumentUpload title="Bachelor Degree" />
+      </Box>
     </Container>
   )
 }
--- src/pages/directory/members/profile/documents/MemberGovDocuments.tsx
+++ src/pages/directory/members/profile/documents/MemberGovDocuments.tsx
@@ -1,9 +1,21 @@
-import { Container, Heading } from '@chakra-ui/react'
+import { Box, Container, Heading } from '@chakra-ui/react'
+import DocumentUpload from './components/DocumentUpload'
+import ProfileAvatar from './components/ProfileAvatar'
+import { RootState } from 'redux-config/store'
+import { useSelector } from 'react-redux'
 
 const MemberGovDocuments = () => {
+  const member = useSelector((state: RootState) => state.member.data)
+
   return (
     <Container>
-      <Heading textAlign={'center'}>Government Documents</Heading>
+      <Heading textAlign={'center'} mb={10}>
+        Government Documents
+      </Heading>
+      <ProfileAvatar member={member} />
+      <Box mt={10}>
+        <DocumentUpload title="Passport" />
+      </Box>
     </Container>
   )
 }
